e9a6c5d04f5ee782cc0bc4d4cea64b5a9fd1e616
“wiatec” <patrickxu@wiatec.com>
2020-04-22 10:56:04 +0800

    202004221055

--- src/main/java/com/ex/lib/http/callback/DownloadCallback.java
+++ src/main/java/com/ex/lib/http/callback/DownloadCallback.java
@@ -4,9 +4,14 @@ package com.ex.lib.http.callback;
 import com.ex.lib.entities.HttpDownloadInfo;
 import okhttp3.Call;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
 
 /**
  * @author patrick
@@ -20,9 +25,92 @@ public abstract class DownloadCallback extends AbstractCallback {
     public abstract void onComplete(HttpDownloadInfo downloadInfo);
     public abstract void onCancel(HttpDownloadInfo downloadInfo);
 
+    private HttpDownloadInfo httpDownloadInfo;
+
+    public void setHttpDownloadInfo(HttpDownloadInfo httpDownloadInfo) {
+        this.httpDownloadInfo = httpDownloadInfo;
+    }
+
     @Override
     public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
         super.onResponse(call, response);
+        if(StringUtils.isEmpty(httpDownloadInfo.getPath())){
+            onFailure(4201, "Download Path is not present");
+            return;
+        }
+        InputStream inputStream =null;
+        RandomAccessFile randomAccessFile = null;
+        try {
+            ResponseBody responseBody = response.body();
+            if(responseBody == null){
+                onFailure(5101, "Response Body Error");
+                return;
+            }
+            if(responseBody.contentLength() <= 0 ){
+                onFailure(5102, "Response File Length Read Error");
+                return;
+            }
+            httpDownloadInfo.setLength(responseBody.contentLength());
+            File dir = new File(httpDownloadInfo.getPath());
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            File file = new File(dir, httpDownloadInfo.getFileName());
+            if(file.exists() && file.length() == httpDownloadInfo.getLength()){
+                httpDownloadInfo.setStatus(1);
+                httpDownloadInfo.setProgress(100);
+                httpDownloadInfo.setMessage("file is exists , no need download");
 
+                return;
+            }
+            randomAccessFile = new RandomAccessFile(file, "rwd");
+            randomAccessFile.seek(httpDownloadInfo.getStartPosition());
+            inputStream = responseBody.byteStream();
+            httpDownloadInfo.setStatus(1);
+            httpDownloadInfo.setMessage("download is start");
+
+            int length = -1;
+            byte[] buffer = new byte[1024 * 1024];
+            long finished = 0;
+            long currentTime =System.currentTimeMillis();
+            while ((length = inputStream.read(buffer)) != -1) {
+                randomAccessFile.write(buffer, 0, length);
+                finished += length;
+                if(System.currentTimeMillis() - currentTime > 2000) {
+                    httpDownloadInfo.setFinishedPosition(finished);
+                    httpDownloadInfo.setProgress((int) (finished * 100L / httpDownloadInfo.getLength()));
+                    httpDownloadInfo.setStatus(1);
+                    httpDownloadInfo.setMessage("downloading");
+
+                    currentTime = System.currentTimeMillis();
+                }
+            }
+            if (file.length() == httpDownloadInfo.getLength()) {
+                httpDownloadInfo.setProgress(100);
+                httpDownloadInfo.setStatus(1);
+                httpDownloadInfo.setMessage("download was finished");
+
+            } else {
+                httpDownloadInfo.setStatus(1);
+                httpDownloadInfo.setMessage("file check error after download");
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            httpDownloadInfo.setStatus(1);
+            httpDownloadInfo.setMessage(e.getMessage());
+
+        }finally {
+            try {
+                if(randomAccessFile !=null) {
+                    randomAccessFile.close();
+                }
+                if(inputStream != null){
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
